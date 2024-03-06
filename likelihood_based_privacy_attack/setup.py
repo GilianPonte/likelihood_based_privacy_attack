062ff82674e11da0c211b1633191990c09ac3b04
Gilian <gilianponte@live.nl>
2024-03-06 13:23:01 +0100

    Update setup.py

--- likelihood_based_privacy_attack/setup.py
+++ likelihood_based_privacy_attack/setup.py
@@ -4,9 +4,9 @@ setup(
     name='likelihood_based_privacy_attack',
     version='0.1',
     packages=find_packages(),
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='Description of your package',
+    author='Gilian',
+    author_email='ponte@rsm.nl',
+    description='Privacy attack',
     url='https://github.com/GilianPonte/likelihood_based_privacy_attack',
     install_requires=[
         'numpy',
