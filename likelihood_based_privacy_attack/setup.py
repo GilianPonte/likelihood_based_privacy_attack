8ab78bf51e49df0e1bf48883454adf4c588079c9
Gilian <gilianponte@live.nl>
2024-03-06 13:21:11 +0100

    Update setup.py

--- likelihood_based_privacy_attack/setup.py
+++ likelihood_based_privacy_attack/setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='privacy_attack',
+    name='likelihood_based_privacy_attack',
     version='0.1',
     packages=find_packages(),
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Description of your package',
+    url='https://github.com/GilianPonte/likelihood_based_privacy_attack',
     install_requires=[
         'numpy',
         'scikit-learn',
+        # Add any other dependencies here
     ],
-    entry_points={
-        'console_scripts': [
-            'privacy_attack=privacy_attack:main',
-        ],
-    },
 )
